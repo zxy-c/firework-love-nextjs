05a7f8af723d9a54275dd2ee4fd08d4b0664c5fb
zxy <2544682494@qq.com>
2023-01-08 10:25:43 +0800

    Rebuild

---
+++ docs/_next/static/x7HJK7xSpxgRwqSQj7N2n/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ef46db3751d8e999.css","static/chunks/pages/index-6a1669040ce0bc42.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
